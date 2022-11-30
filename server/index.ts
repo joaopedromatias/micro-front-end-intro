8a2cf9b281a13dad39080e11c2574259edad3334
joaopedromatias <joaopedromatias@outlook.com>
2022-11-30 08:48:56 -0300

    setup mf projects

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3002;
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.listen(port, () => {
+  console.log(`server listening at http://localhost:${port}`);
+});
